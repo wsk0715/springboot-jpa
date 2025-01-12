1f4c5381bd3ed69fab66606f532a9c1387ddfe84
wsk0715 <leindey@gmail.com>
2025-01-13 02:10:57 +0900

    :sparkles:feat: add exception check feature on JwtInterceptor
    - JwtInterceptor에서 예외 처리하도록 수정

--- src/main/java/com/example/springboot_jpa/auth/interceptor/JwtInterceptor.java
+++ src/main/java/com/example/springboot_jpa/auth/interceptor/JwtInterceptor.java
@@ -2,6 +2,7 @@ package com.example.springboot_jpa.auth.interceptor;
 
 
 import com.example.springboot_jpa.auth.service.AuthService;
+import com.example.springboot_jpa.common.exception.SpringbootJpaException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -24,24 +25,30 @@ public class JwtInterceptor implements HandlerInterceptor {
 	public boolean preHandle(HttpServletRequest request,
 							 HttpServletResponse response,
 							 Object handler) throws Exception {
-		// 쿠키에서 토큰 String 가져오기
-		String token = authService.extractJwtFromRequest(request);
+		try {
+			// 쿠키에서 토큰 String 가져오기
+			String token = authService.extractJwtFromRequest(request);
 
-		// 토큰이 없는 경우 처리
-		if (token == null || token.isEmpty()) {
-			log.warn("쿠키에 JWT 토큰이 존재하지 않습니다.");
-			response.setStatus(HttpStatus.UNAUTHORIZED.value());
+			// 토큰이 없는 경우 처리
+			if (token == null || token.isEmpty()) {
+				log.warn("쿠키에 JWT 토큰이 존재하지 않습니다.");
+				response.setStatus(HttpStatus.UNAUTHORIZED.value());
+				return false;
+			}
+
+			// JWT 토큰 검증
+			if (!authService.validateToken(token)) {
+				log.warn("만료되었거나 유효하지 않은 JWT 토큰입니다.");
+				response.setStatus(HttpStatus.UNAUTHORIZED.value());
+				return false;
+			}
+
+			return true;
+		} catch (SpringbootJpaException e) {
+			log.warn("쿠키 처리 중 예외가 발생했습니다.");
+			response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
 			return false;
 		}
-
-		// JWT 토큰 검증
-		if (!authService.validateToken(token)) {
-			log.warn("만료되었거나 유효하지 않은 JWT 토큰입니다.");
-			response.setStatus(HttpStatus.UNAUTHORIZED.value());
-			return false;
-		}
-
-		return true;
 	}
 
 	@Override
