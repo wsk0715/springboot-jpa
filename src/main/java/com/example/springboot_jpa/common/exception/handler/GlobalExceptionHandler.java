559cff17d99f36d4a26cffa435ab4f524d4bb071
wsk0715 <leindey@gmail.com>
2025-01-20 14:51:57 +0900

    :sparkles:feat: improve global exception handler
    - GlobalExceptionHandler가 예외 스택트레이스를 로그에 남기도록 수정

--- src/main/java/com/example/springboot_jpa/common/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/springboot_jpa/common/exception/handler/GlobalExceptionHandler.java
@@ -16,6 +16,9 @@ public class GlobalExceptionHandler {
 	@Value("${spring.application.name}")
 	private String appName;
 
+	/**
+	 * 일반적인 사용자 정의 예외를 처리하는 핸들러 메소드
+	 */
 	@ExceptionHandler(SpringbootJpaException.class)
 	public ResponseEntity<ErrorResponse> handleSpringbootJpaException(SpringbootJpaException e) {
 		log.warn("[{} Exception] {}: {}", appName, e.getClass().getName(), e.getMessage());
@@ -23,11 +26,22 @@ public class GlobalExceptionHandler {
 							 .body(ErrorResponse.of(e.getMessage(), HttpStatus.BAD_REQUEST.value()));
 	}
 
+	/**
+	 * 예상치 못한 나머지 예외를 처리하는 핸들러 메소드
+	 */
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<ErrorResponse> handleGenericException(Exception e) {
 		log.error("[Unknown Exception] {}: {}", e.getClass().getName(), e.getMessage());
+		logErrorStackTrace(e);
 		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR) // 500 Internal Server Error 응답
 							 .body(ErrorResponse.of("서버 내부 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR.value()));
+	}
+
+	private void logErrorStackTrace(Exception e) {
+		StackTraceElement[] trace = e.getStackTrace();
+		for (StackTraceElement traceElement : trace) {
+			log.error("\tat " + traceElement);
+		}
 	}
 
 }
