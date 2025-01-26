dd48e58d207097c456990b4c7858cbad5767b144
wsk0715 <leindey@gmail.com>
2025-01-27 00:35:07 +0900

    :sparkles:feat: add exception handler to handle HTTP status 403
    - 403(Forbidden) 상태 코드를 처리할 수 있도록 수정

--- src/main/java/com/example/springboot_jpa/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/springboot_jpa/exception/handler/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.example.springboot_jpa.exception.handler;
 
 import com.example.springboot_jpa.exception.handler.response.ErrorResponse;
 import com.example.springboot_jpa.exception.type.SpringbootJpaException;
+import com.example.springboot_jpa.exception.type.status4xx.ForbiddenException;
 import com.example.springboot_jpa.exception.type.status4xx.NotFoundException;
 import com.example.springboot_jpa.exception.type.status4xx.UnauthorizedException;
 import lombok.extern.slf4j.Slf4j;
@@ -20,7 +21,7 @@ public class GlobalExceptionHandler {
 
 
 	/**
-	 * 400 Bad Request
+	 * 400 Bad Request<br>
 	 * 사용자 정의 예외를 처리하는 핸들러 메소드
 	 */
 	@ExceptionHandler(SpringbootJpaException.class)
@@ -29,7 +30,7 @@ public class GlobalExceptionHandler {
 	}
 
 	/**
-	 * 401 Unauthorized
+	 * 401 Unauthorized<br>
 	 * 인증되지 않은 사용자가 인증이 필요한 항목에 접근한 경우의 예외를 처리하는 핸들러 메소드
 	 */
 	@ExceptionHandler(UnauthorizedException.class)
@@ -38,7 +39,16 @@ public class GlobalExceptionHandler {
 	}
 
 	/**
-	 * 404 Not Found
+	 * 403 Forbidden<br>
+	 * 요청이 권한을 벗어났을 경우의 예외를 처리하는 핸들러 메소드
+	 */
+	@ExceptionHandler(ForbiddenException.class)
+	public ResponseEntity<ErrorResponse> handleForbidden(ForbiddenException e) {
+		return logWarnAndCreateResponse(e, HttpStatus.FORBIDDEN);
+	}
+
+	/**
+	 * 404 Not Found<br>
 	 * 요청한 콘텐츠를 찾지 못했을 경우의 예외를 처리하는 핸들러 메소드
 	 */
 	@ExceptionHandler(NotFoundException.class)
@@ -47,7 +57,7 @@ public class GlobalExceptionHandler {
 	}
 
 	/**
-	 * 500 Internal Server Error
+	 * 500 Internal Server Error<br>
 	 * 예외 처리를 하지 못한 나머지 예외를 처리하는 핸들러 메소드
 	 */
 	@ExceptionHandler(Exception.class)
