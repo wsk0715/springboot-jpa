922ec3569b0ce8573829e82cb1cc56dda0926d2a
wsk0715 <leindey@gmail.com>
2025-02-02 23:33:02 +0900

    :bug:fix: fix request dto validation issue
    - 요청 DTO 유효성 검증 어노테이션 수정

--- src/main/java/com/example/springboot_jpa/user/controller/request/UserRequest.java
+++ src/main/java/com/example/springboot_jpa/user/controller/request/UserRequest.java
@@ -1,17 +1,17 @@
-package com.example.springboot_jpa.user.contoller.request;
+package com.example.springboot_jpa.user.controller.request;
 
 import com.example.springboot_jpa.user.domain.User;
 import com.example.springboot_jpa.user.domain.vo.Nickname;
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Schema(description = "회원 도메인에 사용되는 요청 형식")
 public record UserRequest(
 		@Schema(example = "방가맨")
 		@NotBlank(message = Nickname.NICKNAME_BLANK_MESSAGE)
-		@Max(value = Nickname.NICKNAME_MAX_LENGTH,
-			 message = Nickname.NICKNAME_LENGTH_MESSAGE
+		@Size(max = Nickname.NICKNAME_MAX_LENGTH,
+			  message = Nickname.NICKNAME_LENGTH_MESSAGE
 		)
 		String nickname
 ) {
