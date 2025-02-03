b255af00628ba11ecb642abbdf08033155c16fd7
wsk0715 <leindey@gmail.com>
2025-02-03 21:47:28 +0900

    :sparkles:feat: add search feature with title on GET /board/
    - GET /board/ 요청에 게시글 제목 검색기능 추가

--- src/main/java/com/example/springboot_jpa/board/controller/BoardController.java
+++ src/main/java/com/example/springboot_jpa/board/controller/BoardController.java
@@ -48,11 +48,12 @@ public class BoardController implements BoardControllerDocs {
 	@GetMapping
 	public ResponseEntity<ResponsePage<BoardResponseSummary>> getBoards(@RequestParam(required = false) Long userId,
 																		@RequestParam(required = false) String userNickname,
+																		@RequestParam(required = false) String title,
 																		@RequestParam(defaultValue = "1") int page,
 																		@RequestParam(defaultValue = "20") int size,
 																		@RequestParam(defaultValue = "id, desc") String sort) {
 		Pageable pageable = PaginationUtil.createPageable(page, size, sort);
-		Page<Board> boards = boardService.getBoards(userId, userNickname, pageable);
+		Page<Board> boards = boardService.getBoards(userId, userNickname, title, pageable);
 		List<BoardResponseSummary> boardSummaries = BoardResponseSummary.from(boards.getContent());
 		ResponsePage<BoardResponseSummary> res = ResponsePage.from(boardSummaries,
 																   page,
